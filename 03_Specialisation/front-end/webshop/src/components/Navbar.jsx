ed29b388537c96484e1dd096424a7c7dfad50c10
Sander_iMac <sander@grunnekemeijer.net>
2022-08-25 16:50:58 +0200

    first steps

---
+++ 03_Specialisation/front-end/webshop/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Container, Nav, Navbar as NavbarBS } from "react-bootstrap"
+import { NavLink } from "react-router-dom"  
+// imported bootstrap navbar 
+export function Navbar () {
+    return (
+    <NavbarBS className="bg-white shadow-sm mb-3">
+        <Container>
+            <Nav className="me-auto">
+                <Nav.Link to="/" as={NavLink}>
+                Home
+                </Nav.Link>
+                <Nav.Link to="/Store" as={NavLink}>
+                Store
+                </Nav.Link>
+            </Nav>
+            <button></button>
+        </Container>
+    </NavbarBS>
+
+
+
+    )
+};
+
+
+
+
