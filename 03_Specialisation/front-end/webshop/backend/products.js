25e72d5b44cc01e3f83537a00a7f4bc442a9d1af
Sander_iMac <sander@grunnekemeijer.net>
2022-09-19 16:29:20 +0200

    productlist  aangevuld

--- 03_Specialisation/front-end/webshop/backend/products.js
+++ 03_Specialisation/front-end/webshop/backend/products.js
@@ -1,54 +1,114 @@
 const products = [
 
-    
-        {
-          id: "1",
-          title: "Skateboard 1",
-          desc: "This is a description of the skateboard nr. 1 This is a description of the skateboard nr. 1 ",
-          category: "NewArrival",
-          price: "100",
-          image: "https://res.cloudinary.com/ordinator/image/upload/v1662468986/SkippySkateboards/product5_zvb9i1.jpg",
-        },
-        {
-          id: "2",
-          title: "Skateboard 2",
-          desc: "This is a description of the skateboard nr. 2 This is a description of the skateboard nr. 2",
-          category: "Vintage",
-          price: "100",
-          image: "https://res.cloudinary.com/ordinator/image/upload/v1662468986/SkippySkateboards/product4_kmwiws.jpg",
-        },
-        {
-          id: "3",
-          title: "Skateboard 3",
-          desc: "This is a description of the skateboard nr. 3 This is a description of the skateboard nr. 3",
-          category: "Reissue",
-          price: "100",
-          image: "https://res.cloudinary.com/ordinator/image/upload/v1662468986/SkippySkateboards/product6_vpuyoh.jpg",
-        },
-        {
-          id: "4",
-          title: "Skateboard 4",
-          desc: "This is a description of the skateboard nr. 4",
-          category: "Vintage",
-          price: "100",
-          image: "https://res.cloudinary.com/ordinator/image/upload/v1662468986/SkippySkateboards/product2_b1axxe.jpg",
-        },
-        {
-          id: "5",
-          title: "Skateboard 5",
-          desc: "This is a description of the skateboard nr. 5",
-          category: "Vintage",
-          price: "100",
-          image: "https://res.cloudinary.com/ordinator/image/upload/v1662468986/SkippySkateboards/product3_jg6xhh.jpg",
-        },
-        {
-          id: "6",
-          title: "Skateboard 6",
-          desc: "This is a description of the skateboard nr. 6",
-          category: "Parts",
-          price: "100",
-          image: "https://res.cloudinary.com/ordinator/image/upload/v1662468986/SkippySkateboards/product3_jg6xhh.jpg",
-        }
+
+  {
+    id: "1",
+    title: "KRUX K5 MARBIE MILLER DLK 8.5 SKATEBOARD TRUCK",
+    desc: "Get creative just like Marbie Miller on the Krux 8.50 K5 Marbie Miller DLK Skateboard Truck!",
+    category: "Parts",
+    price: "38.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663597410/SkippySkateboards/1_twpie2.jpg",
+  },
+  {
+    id: "2",
+    title: "INDEPENDENT 149 STAGE 11 SLAYER SKATEBOARD TRUCK",
+    desc: "Made in special collaboration with heavy metal band Slayer.",
+    category: "Parts",
+    price: "39.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663597410/SkippySkateboards/2_rk3he9.jpg",
+  },
+  {
+    id: "3",
+    title: "ACE TRUCKS 60 AF1 SKATEBOARD TRUCK",
+    desc: "Years in development, the ACE AF1 has a 70% increase in structural integrity, making it the strongest cast Truck on the market!",
+    category: "Parts",
+    price: "37.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663597410/SkippySkateboards/3_itvbdx.jpg",
+  },
+  {
+    id: "4",
+    title: "GRIND KING DISRUPTOR TRUCK 6.0",
+    desc: "Grind King is back with the Disruptor Truck to take your skating to the next level and give you the smoothest grinds!",
+    category: "Parts",
+    price: "27.50",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663597410/SkippySkateboards/4_nbvyuw.jpg",
+  },
+  {
+    id: "5",
+    title: "MOB SKATEBOARD GRIPTAPE 10 Inch",
+    desc: "MOB Grip tape 10 Inch - Black",
+    category: "Parts",
+    price: "8.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663596007/SkippySkateboards/5_wvjicp.jpg",
+  },
+  {
+    id: "6",
+    title: "POWELL-PERALTA RIPPER CHARCOAL CHECKER GRIPTAPE",
+    desc: "Ripper Charcoal Checker Grip 9 Inch griptape sheet for your skateboard deck",
+    category: "Parts",
+    price: "18.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663595490/SkippySkateboards/6_d8j6na.jpg",
+  },
+  {
+    id: "7",
+    title: "PRIMITIVE VIANNA BLISS 8.38",
+    desc: "Surf the vaporwave with this beautiful board.",
+    category: "Reissue",
+    price: "76.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663594202/SkippySkateboards/7_b1lzkx.jpg",
+  },
+  {
+    id: "8",
+    title: "ALMOST YOUNESS REN & STIMPY ROOM MATE R7",
+    desc: "Has a Width of 8 and a Length of 31.7 inch. This deck is made in special collaboration with the infamous animated show, Ren & Stimpy.",
+    category: "Reissue",
+    price: "59.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663594203/SkippySkateboards/8_mcrk8m.jpg",
+  },
+  {
+    id: "9",
+    title: "SANTA CRUZ SCREAMING HAND 8.80",
+    desc: "The Santa Cruz Screaming Hand Skateboard Deck. One of the most famous graphics in skateboarding.",
+    category: "Reissue",
+    price: "79.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663594203/SkippySkateboards/9_knzyr4.jpg",
+  },
+  {
+    id: "10",
+    title: "POWELL-PERALTA FLIGHT CABALLERO FACTION",
+    desc: "Powell-Peralta FLIGHT Caballero Faction II SkateboardDeck.",
+    category: "Reissue",
+    price: "134.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663594203/SkippySkateboards/10_m8fyn7.jpg",
+  },
+  {
+    id: "11",
+    title: "RIPNDIP LORD NERMAL FUCKIN FUCK 8.5",
+    desc: "Width: 21.6cm / 8.5 Inch - Made in USA",
+    category: "Reissue",
+    price: "79.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663594203/SkippySkateboards/11_z1p1fa.jpg",
+  },
+  {
+    id: "12",
+    title: "BONES SUPER SWISS 6 BEARINGS",
+    desc: "Bones Super Swiss, 6balls Bearings, built with swiss precision in Switzerland. Bones Swiss 6 - 8mm Axle - Spacers Included",
+    category: "Parts",
+    price: "129.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663594203/SkippySkateboards/12_qwqnxg.jpg",
+  },
+  {
+    id: "13",
+    title: "BRONSON G3 SPEED CO BEARING",
+    desc: "Steel Bearings. Regular skateboard Bearings (no Built- in style) Size: 8 mm Pre-lubricated Removable bearing shields Including metal storage box, bearing spacers and speed rings",
+    category: "Parts",
+    price: "42.95",
+    image: "https://res.cloudinary.com/ordinator/image/upload/v1663594203/SkippySkateboards/13_laz8gn.jpg",
+  },
+
+
+
+
 
 ];
 
