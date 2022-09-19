20fa5d1a1d8512653a65e53982c5047a2b57a6de
Sander_iMac <sander@grunnekemeijer.net>
2022-09-19 11:45:41 +0200

    404 message styling

--- 03_Specialisation/front-end/webshop/src/components/NotFound/NotFound.js
+++ 03_Specialisation/front-end/webshop/src/components/NotFound/NotFound.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import styled from 'styled-components';
+import four from "../../images/four.png"
+import zero from "../../images/zero.png"
+
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <>
     <Not_Found>
-    <h1>404</h1>
-    <h2>Page not found...</h2> 
+    <Numbers>
+    <Four/><Zero/><Four/>
+    </Numbers>
+    <ErrorMsg>Page not found...</ErrorMsg> 
+
+    <StartShopping>
+                  <Link to="/"><h4>Continue Shopping..</h4></Link>
+                </StartShopping>
     </Not_Found>
     </>
   )
@@ -23,4 +33,41 @@ align-items: center;
 justify-content: center;
 color: rgb(68,68,68);
 min-height: 80vh;
-`
+`;
+
+const Numbers = styled.div`
+display: flex;
+flex-direction: row;
+`;
+
+const Four = styled.img.attrs({
+  src: `${four}`
+})`
+max-height: 20vh;
+display:flex;
+`;
+
+const Zero = styled.img.attrs({
+  src: `${zero}`
+})`
+max-height: 20vh;
+display:flex;
+`;
+
+const ErrorMsg = styled.h2`
+color: black;
+`;
+
+const StartShopping = styled.div`
+  font-size: 14px;
+  
+  font-weight: 600;
+  & h4 {
+    color: #d7732b;
+    text-shadow: none;
+  };
+`;
+
+
+
+
